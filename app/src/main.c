8a639259611736b2270c9f732f45dc063808625a
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2025-10-16 20:20:03 -0600

    Button pressed with EiE API

--- app/src/main.c
+++ app/src/main.c
@@ -3,53 +3,31 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
+
+#include "BTN.h"
+#include "LED.h"
 #include <zephyr/sys/printk.h>
-#include <inttypes.h>
 
-#define SLEEP_TIME_MS 1000
-
-#define SWO_NODE DT_ALIAgS(sw0)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SWO_NODE, gpios);
-
-static struct gpio_callback button_isr_data;
-
-static uint8_t presses = 0;
-
-void button_isr(const struct device* dev, struct gpio_callback* cb, uint32_t pins)
-{
-    printk("Button 0 Pressed! (time %d)\n", presses);
-    presses++;
-}
-
+#define SLEEP_MS 1
 
 int main(void)
 {
-    int ret;
-
-    if (!gpio_is_ready_dt(&button))
+    if (BTN_init() < 0)
     {
         return 0;
     }
-
-    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-    if (ret < 0)
+    if (LED_init() < 0)
     {
         return 0;
     }
-
-    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-    if (ret < 0)
-    {
-        return 0;
-    }
-
-    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
-    gpio_add_callback(button.port, &button_isr_data);
 
     while (1) {
-
+        if (BTN_check_clear_pressed(BTN0))
+        {
+            LED_toggle(LED0);
+            printk("Button 0 pressed!\n");
+        }
+        k_msleep(SLEEP_MS);
     }
     return 0;
 }
