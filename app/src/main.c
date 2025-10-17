03a150572c96062963bef50da91273906e2cae48
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2025-10-16 20:02:05 -0600

    Manual interrupt

--- app/src/main.c
+++ app/src/main.c
@@ -13,6 +13,16 @@
 #define SWO_NODE DT_ALIAgS(sw0)
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SWO_NODE, gpios);
 
+static struct gpio_callback button_isr_data;
+
+static uint8_t presses = 0;
+
+void button_isr(const struct device* dev, struct gpio_callback* cb, uint32_t pins)
+{
+    printk("Button 0 Pressed! (time %d)\n", presses);
+    presses++;
+}
+
 
 int main(void)
 {
@@ -29,13 +39,17 @@ int main(void)
         return 0;
     }
 
+    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+    if (ret < 0)
+    {
+        return 0;
+    }
+
+    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
+    gpio_add_callback(button.port, &button_isr_data);
+
     while (1) {
-        ret = gpio_pin_get_dt(&button);
-        if (ret > 0)
-        {
-            printk("Pressed!\n");
-        }
-        k_msleep(SLEEP_TIME_MS);
+
     }
     return 0;
 }
