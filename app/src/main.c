3bdcb1b0661358cdf5e3077d04c453c6789b8aff
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2025-10-16 21:27:48 -0600

    Makes LED blink as counter

--- app/src/main.c
+++ app/src/main.c
@@ -21,10 +21,32 @@ int main(void)
         return 0;
     }
 
+    led_id LEDs[] = {
+        LED0,
+        LED1,
+        LED2,
+        LED3
+    };
+
+    uint8_t counter = 0;
+
     while (1) {
         if (BTN_check_clear_pressed(BTN0))
         {
-            LED_toggle(LED0);
+            counter++;
+            uint8_t mask = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                if (counter & mask)
+                {
+                    LED_set(LEDs[i], LED_ON);
+                }
+                else
+                {
+                    LED_set(LEDs[i], LED_OFF);
+                }
+                mask <<= 1;
+            }
             printk("Button 0 pressed!\n");
         }
         k_msleep(SLEEP_MS);
