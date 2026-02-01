f5076041acfd16c8535d04e3c6ef85b5b29e53db
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2026-02-01 16:16:52 -0700

    Allows switching between multiple screens

--- app/src/main.c
+++ app/src/main.c
@@ -23,11 +23,13 @@
 #define NUM_BUTTONS 4
 
 static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
-static lv_obj_t *screen = NULL;
+static lv_obj_t* buttonScreen;
+static lv_obj_t* imageScreen;
 
 extern const lv_image_dsc_t UCalgary_Vertical_logo_colour_1;
 
 void button_callback(lv_event_t *event) {
+  lv_screen_load(imageScreen);
   lv_obj_t* data_obj = (lv_obj_t*) lv_event_get_user_data(event);
   uint8_t* user_data = (uint8_t*) lv_data_obj_get_data_ptr(data_obj);
   printk("Button %u Pressed\n", *user_data);
@@ -45,16 +47,12 @@ int main(void) {
   if (!device_is_ready(display_dev)) {
     return 0;
   }
+
+  buttonScreen = lv_obj_create(NULL);
+  imageScreen = lv_obj_create(NULL);
   
-  // Create LVGL screen
-  screen = lv_screen_active();
-  if (screen == NULL) {
-    return 0;
-  }
-  
-#if 0
   for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
-    lv_obj_t *button = lv_button_create(screen);                                        // Create button as child of screen
+    lv_obj_t *button = lv_button_create(buttonScreen);                                        // Create button as child of screen
     lv_obj_align(button, LV_ALIGN_CENTER, i % 2 ? 50 : -50, i < 2 ? -20 : 20);          // Align in 2x2 grid
     lv_obj_t* user_data = lv_data_obj_create_alloc_assign(button, &i, sizeof(i));       // Associate button number information with button
     lv_obj_add_event_cb(button, button_callback, LV_EVENT_CLICKED, user_data);          // Add callback function on button press
@@ -64,13 +62,15 @@ int main(void) {
     sprintf(label_text, "Button %u", i);        // Write text into buffer
     lv_label_set_text(label, label_text);           // Set label text
   }
-#endif
 
-  lv_obj_t* image = lv_image_create(screen);
+  lv_obj_t* image = lv_image_create(imageScreen);
   lv_image_set_src(image, &UCalgary_Vertical_logo_colour_1);
   lv_obj_align(image, LV_ALIGN_CENTER, 0, 0);
 
   display_blanking_off(display_dev);
+
+  lv_screen_load(buttonScreen);
+
   while (1) {
     lv_timer_handler();
     k_msleep(SLEEP_MS);
