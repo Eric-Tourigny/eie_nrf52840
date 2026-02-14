c395ed8e9863f69ccff8a9e04d279148cad11732
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2026-02-14 10:12:29 -0700

    Adds example gatt-server

--- gatt-server/gatt-server.py
+++ gatt-server/gatt-server.py
@@ -1,118 +1,82 @@
 """
-Based on example for a BLE 4.0 Server using a GATT dictionary of services and characteristics
-https://github.com/kevincar/bless/blob/master/examples/gattserver.py
+Example for a BLE 4.0 Server
 """
 
-import asyncio
-import logging
 import sys
+import logging
+import asyncio
 import threading
 
-from typing import Any, Dict, Union
+from typing import Any, Union
 
-from bless import BlessServer, BlessGATTCharacteristic, GATTCharacteristicProperties, GATTAttributePermissions, BlessGATTRequest, BlessGATTSession #type: ignore
+from bless import (  # type: ignore
+    BlessServer,
+    BlessGATTCharacteristic,
+    GATTCharacteristicProperties,
+    GATTAttributePermissions,
+)
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(name=__name__)
 
-trigger: Union[asyncio.Event, threading.Event] = (
-    threading.Event() if sys.platform in ["darwin", "win32"] else asyncio.Event()
-)
+# NOTE: Some systems require different synchronization methods.
+trigger: Union[asyncio.Event, threading.Event]
+if sys.platform in ["darwin", "win32"]:
+    trigger = threading.Event()
+else:
+    trigger = asyncio.Event()
 
 
-def on_read(
-    characteristic: BlessGATTCharacteristic, request: BlessGATTRequest
-) -> bytearray:
+def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
     logger.debug(f"Reading {characteristic.value}")
     return characteristic.value
 
 
-def on_write(
-    characteristic: BlessGATTCharacteristic, value: Any, request: BlessGATTRequest
-):
+def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
     characteristic.value = value
     logger.debug(f"Char value set to {characteristic.value}")
     if characteristic.value == b"\x0f":
-        logger.debug("Nice")
+        logger.debug("NICE")
         trigger.set()
-
-
-def on_subscribe(characteristic: BlessGATTCharacteristic, session: BlessGATTSession):
-    logger.debug(f"Subscribed to {characteristic.uuid}")
-
-
-def on_unsubscribe(characteristic: BlessGATTCharacteristic, session: BlessGATTSession):
-    logger.debug(f"Unsubscribed from {characteristic.uuid}")
-
-
-# Characteristic-specific handlers
-def inc(c: BlessGATTCharacteristic, req: BlessGATTRequest) -> bytearray:
-    c.value = c.value if c.value is not None else bytearray("\x00")
-    n: int = int.from_bytes(bytes(c.value))
-    c.value = bytearray((n + 1).to_bytes())
-    return c.value
 
 
 async def run(loop):
     trigger.clear()
-
     # Instantiate the server
-    gatt: Dict = {
-        "A07498CA-AD5B-474E-940D-16F1FBE7E8CD": {
-            "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B": {
-                "Properties": (
-                    GATTCharacteristicProperties.read
-                    | GATTCharacteristicProperties.write
-                    | GATTCharacteristicProperties.indicate
-                ),
-                "Permissions": (
-                    GATTAttributePermissions.readable
-                    | GATTAttributePermissions.writeable
-                ),
-                "Value": None,
-            }
-        },
-        "5c339364-c7be-4f23-b666-a8ff73a6a86a": {
-            "bfc0c92f-317d-4ba9-976b-cc11ce77b4ca": {
-                "Properties": GATTCharacteristicProperties.read,
-                "Permissions": GATTAttributePermissions.readable,
-                "Value": None,
-                "OnRead": inc,
-            }
-        },
-    }
     my_service_name = "Test Service"
-    server = BlessServer(
-        name=my_service_name,
-        loop=loop,
-        on_read=on_read,
-        on_write=on_write,
-        on_subscribe=on_subscribe,
-        on_unsubscribe=on_unsubscribe,
+    server = BlessServer(name=my_service_name, loop=loop)
+    server.read_request_func = read_request
+    server.write_request_func = write_request
+
+    # Add Service
+    my_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
+    await server.add_new_service(my_service_uuid)
+
+    # Add a Characteristic to the service
+    my_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+    char_flags = (
+        GATTCharacteristicProperties.read
+        | GATTCharacteristicProperties.write
+        | GATTCharacteristicProperties.indicate
+    )
+    permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
+    await server.add_new_characteristic(
+        my_service_uuid, my_char_uuid, char_flags, None, permissions
     )
 
-    await server.add_gatt(gatt)
+    logger.debug(server.get_characteristic(my_char_uuid))
     await server.start()
-    logger.debug(server.get_characteristic("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"))
     logger.debug("Advertising")
-    logger.info(
-        "Write '0xF' to the advertised characteristic: "
-        + "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    )
-    if isinstance(trigger, threading.Event):
+    logger.info(f"Write '0xF' to the advertised characteristic: {my_char_uuid}")
+    if trigger.__module__ == "threading":
         trigger.wait()
     else:
         await trigger.wait()
-    logger.info("Triggered... Waiting 2 seconds")
-    await asyncio.sleep(2)
 
-    logger.debug("Updating characteristic")
-    server.get_characteristic("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B").value = bytearray(
-        b"i"
-    )
-    server.update_value(
-        "A07498CA-AD5B-474E-940D-16F1FBE7E8CD", "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    )
+    await asyncio.sleep(2)
+    logger.debug("Updating")
+    server.get_characteristic(my_char_uuid)
+    server.update_value(my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
     await asyncio.sleep(5)
     await server.stop()
 
