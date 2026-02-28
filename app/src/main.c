bddf3bf2e33d8c8132ea1ddd2c6994a20a0eb454
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2026-02-28 11:28:47 -0700

    Adds other game objects

--- app/src/main.c
+++ app/src/main.c
@@ -29,31 +29,74 @@
 // Values should be shifted by SUBPIXEL shift before being passed into LVGL
 #define SUBPIXEL_SHIFT 8
 
+#define MAX_NUM_GAME_OBJECTS 100
+
 #define SCREEN_WIDTH (320 << SUBPIXEL_SHIFT)
 #define SCREEN_HEIGHT (240 << SUBPIXEL_SHIFT)
 
-struct {
+struct __vector2_t {
   int32_t x;
   int32_t y;
 } typedef vector2_t;
 
-struct {
-  lv_obj_t* player_body;
-  vector2_t position;
-  vector2_t velocity;
-  uint32_t height;
-  uint32_t width;
+struct __game_object_t {
+  lv_obj_t* image;
+  vector2_t pos;
+  int32_t w;
+  int32_t h;
+} typedef game_object_t;
+
+struct __player_t {
+  game_object_t* obj;
+  vector2_t vel;
 } typedef player_t;
+
 
 static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));   // Find display device used by LVGL
 static lv_obj_t* game_screen;                                                         // LVGL screen on which main game is displayed
-static player_t player = {
-  NULL,
-  {20, 20},
-  {0, 2},
-  10 << SUBPIXEL_SHIFT,
-  10 << SUBPIXEL_SHIFT
-};
+
+static player_t player;
+
+static game_object_t* game_objects[MAX_NUM_GAME_OBJECTS];
+
+game_object_t* create_game_object(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
+  game_object_t* obj = k_malloc(sizeof(game_object_t));
+  obj->pos.x = x << SUBPIXEL_SHIFT;
+  obj->pos.y = y << SUBPIXEL_SHIFT;
+  obj->w = w << SUBPIXEL_SHIFT;
+  obj->h = h << SUBPIXEL_SHIFT;
+
+  lv_obj_t* image = lv_obj_create(game_screen);
+  lv_obj_set_pos(image, x, y);
+  lv_obj_set_size(image, w, h);
+  lv_obj_set_style_bg_color(image, lv_color_hex(0xff0000), LV_PART_MAIN);
+  lv_obj_set_style_radius(image, 0, LV_PART_MAIN);
+  obj->image = image;
+
+  return obj;
+}
+
+void destroy_game_object(game_object_t* obj) {
+  lv_obj_delete_async(obj->image);
+  k_free(obj);
+}
+
+void init_player() {
+  player.obj = create_game_object(20, 10, 10, 10);
+  player.vel.x = 0;
+  player.vel.y = 0;
+}
+
+uint8_t init_screen() {
+  lv_obj_clean(game_screen);
+
+  init_player();
+
+  game_objects[0] = create_game_object(5, 100, 100, 5);
+  game_objects[1] = create_game_object(120, 90, 50, 5);
+
+  return 0;
+}
 
 uint8_t init_game() {
   if (!device_is_ready(display_dev)) {
@@ -61,46 +104,38 @@ uint8_t init_game() {
     return 1;
   }
 
-
   game_screen = lv_obj_create(NULL);                                      // Create LVGL screen
-
-  lv_obj_t* rect = lv_obj_create(game_screen);                            // Create rectangle LVGL object
-  lv_obj_set_size(rect, player.width >> SUBPIXEL_SHIFT, player.height >> SUBPIXEL_SHIFT);           // Set size as 10 px by 10 px
-  lv_obj_set_pos(rect, player.position.x >> SUBPIXEL_SHIFT, player.position.y >> SUBPIXEL_SHIFT);   // Initial position from top left corner
-  lv_obj_set_style_bg_color(rect, lv_color_hex(0xff0000), LV_PART_MAIN);  // Set color as red
-  lv_obj_set_style_radius(rect, 0, LV_PART_MAIN);                         // Remove circular corners
-  player.player_body = rect;                                              // Set player body as rectangle
 
   return 0;
 }
 
 uint8_t update_game() {
-  player.velocity.y += 64;
+  player.vel.y += 64;
   switch (joystick_state.h) {
     case JOYSTICK_LEFT:
-      player.velocity.x = -256;
+      player.vel.x = -256;
       break;
     case JOYSTICK_HORIZONTAL_NEUTRAL:
-      player.velocity.x = 0;
+      player.vel.x = 0;
       break;
     case JOYSTICK_RIGHT:
-      player.velocity.x = 256;
+      player.vel.x = 256;
       break;
   }
 
 
-  player.position.x += player.velocity.x;
-  player.position.y += player.velocity.y;
-  if (player.position.y > SCREEN_HEIGHT - player.height) {
-    player.position.y = SCREEN_HEIGHT - player.height;
-    player.velocity.y = 0;
+  player.obj->pos.x += player.vel.x;
+  player.obj->pos.y += player.vel.y;
+  if (player.obj->pos.y > SCREEN_HEIGHT - player.obj->h) {
+    player.obj->pos.y = SCREEN_HEIGHT - player.obj->h;
+    player.vel.y = 0;
   }
 
-  if (player.position.y == SCREEN_HEIGHT - player.height && button_check_held(BUTTON_ID_A)) {
-    player.velocity.y = -1024;
+  if (player.obj->pos.y == SCREEN_HEIGHT - player.obj->h && button_check_held(BUTTON_ID_A)) {
+    player.vel.y = -1024;
   }
 
-  lv_obj_set_pos(player.player_body, player.position.x >> SUBPIXEL_SHIFT, player.position.y >> SUBPIXEL_SHIFT);
+  lv_obj_set_pos(player.obj->image, player.obj->pos.x >> SUBPIXEL_SHIFT, player.obj->pos.y >> SUBPIXEL_SHIFT);
 
 
   return 0;
@@ -127,6 +162,7 @@ int main(void) {
 
 
   display_blanking_off(display_dev);    // Turn on screen
+  init_screen();
   lv_screen_load(game_screen);          // Display main game screen
 
   while(1) {
