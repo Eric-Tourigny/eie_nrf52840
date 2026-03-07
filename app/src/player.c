ce3d794fcb1604d5a0e9439c9ad092d8c79599b9
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2026-03-07 09:43:25 -0700

    Greatly simplifies collision detection system

--- app/src/player.c
+++ app/src/player.c
@@ -40,26 +40,25 @@ void update_player_location(game_object_t* colliders, uint32_t num_colliders) {
     game_object_t* obj = &colliders[i];
 
     bool is_right_of_left        = new_x + player.obj.w > obj->pos.x;
-    bool is_left_of_left_hitbox  = new_x + player.obj.w < obj->pos.x + HORIZONTAL_HITBOX_SUBPIXEL_THICKNESS;
-    bool was_left_of_left        = player.obj.pos.x + player.obj.w <= obj->pos.x;
-
     bool is_left_of_right        = new_x < obj->pos.x + obj->w;
-    bool is_right_of_right_hitbox= new_x > obj->pos.x + obj->w - HORIZONTAL_HITBOX_SUBPIXEL_THICKNESS;
-    bool was_right_of_right      = player.obj.pos.x >= obj->pos.x + obj->w;
+    bool within_horizontal_range = is_right_of_left && is_left_of_right;
 
     bool is_below_top            = player.obj.pos.y + player.obj.h > obj->pos.y;
     bool is_above_bottom         = player.obj.pos.y < obj->pos.y + obj->h;
     bool within_vertical_range   = is_below_top && is_above_bottom;
 
+    bool is_moving_right         = player.vel.x > 0;
 
-    if (is_right_of_left && is_left_of_left_hitbox && within_vertical_range && was_left_of_left)            // Collision with the left wall
+    if (within_horizontal_range && within_vertical_range)            
     {
-      new_x = obj->pos.x - player.obj.w;
-      printk("left hitbox");
-    }
-    else if (is_left_of_right && is_right_of_right_hitbox && within_vertical_range && was_right_of_right)   // Collision with right wall
-    {
-      new_x = obj->pos.x + obj->w;
+      if (is_moving_right)                                      // Collision with the left wall
+      {
+        new_x = obj->pos.x - player.obj.w;
+      }
+      else                                                      // Collision with right wall
+      {
+        new_x = obj->pos.x + obj->w;
+      }
     }
   }
 
@@ -78,24 +77,27 @@ void update_player_location(game_object_t* colliders, uint32_t num_colliders) {
     bool within_horizontal_range = is_right_of_left && is_left_of_right;
 
     bool is_below_top            = new_y + player.obj.h > obj->pos.y;
-    bool is_above_top_hitbox     = new_y + player.obj.h < obj->pos.y + VERTICAL_HITBOX_SUBPIXEL_THICKNESS;
-
     bool is_above_bottom         = new_y < obj->pos.y + obj->h;
-    bool is_below_bottom_hitbox  = new_y > obj->pos.y + obj->h - VERTICAL_HITBOX_SUBPIXEL_THICKNESS;
+    bool within_vertical_range   = is_below_top && is_above_bottom;
 
-    if (is_below_top && is_above_top_hitbox && within_horizontal_range)                                // Standing on the ground
+    bool is_moving_down          = player.vel.y > 0;
+
+    if (within_horizontal_range && within_vertical_range)                                
     {
-      if (button_check_clear_pressed(BUTTON_ID_B)) {
-        player.vel.y = -JUMP_SPEED;
-      } else {
-        player.vel.y = 0;
+      if (is_moving_down)               // Standing on the ground
+      {
+        if (button_check_clear_pressed(BUTTON_ID_B)) {
+          player.vel.y = -JUMP_SPEED;
+        } else {
+          player.vel.y = 0;
+        }
+        new_y = obj->pos.y - player.obj.h;
       }
-      new_y = obj->pos.y - player.obj.h;
-    }
-    else if (is_above_bottom && is_below_bottom_hitbox && within_horizontal_range)                          // Hitting the ceiling
-    {
-      player.vel.y = 0;
-      new_y = obj->pos.y + obj->h;
+      else                              // Hitting the ceiling
+      {
+        player.vel.y = 0;
+        new_y = obj->pos.y + obj->h;
+      }
     }
   }
 
