34374d63fff84b05fe8f354a4e76b3bf828db4cc
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2026-03-12 15:24:52 -0600

    Improves coin counter

--- app/src/coins.c
+++ app/src/coins.c
@@ -2,17 +2,54 @@
 #include "screen.h"
 #include <lvgl.h>
 
-lv_obj_t* coins_label;
-int32_t coin_count;
+/*
+ * Update the text displaying how many coins have been collected
+ */
+static void update_coin_counter();
+
+extern const lv_image_dsc_t SpriteCoin;
+
+// Objects involved in coin display
+static lv_obj_t* coin_display;
+static lv_obj_t* coins_label;
+static lv_obj_t* coin_image;
+
+// The current number of coins the player has collected
+static int32_t coin_count = 0;
+
+void update_coin_counter() {
+    lv_label_set_text_fmt(coins_label, "%02d", coin_count);
+}
 
 void init_coin_display(lv_obj_t* game_screen) {
-    coins_label = lv_label_create(lv_layer_top());
-    lv_obj_align(coins_label, LV_ALIGN_TOP_RIGHT, -COINS_LABEL_PX_PADDING, COINS_LABEL_PX_PADDING);     // Place display in top right
-    lv_label_set_text(coins_label, "00");                                                               // Set initial coins as 00
-    lv_obj_set_style_bg_color(coins_label, lv_color_hex(0x000000), LV_PART_MAIN);
+    // Create the transparent-gray component holding the coin display and place it in the top right
+    coin_display = lv_obj_create(lv_layer_top());
+    lv_obj_set_style_border_width(coin_display, 0, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(coin_display, lv_color_hex(0x000000), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(coin_display, LV_OPA_50, LV_PART_MAIN);
+    lv_obj_align(coin_display, LV_ALIGN_TOP_RIGHT, -COINS_LABEL_PX_MARGIN, COINS_LABEL_PX_MARGIN);
+
+    // Size to hold children
+    lv_obj_set_style_width(coin_display, LV_SIZE_CONTENT, LV_PART_MAIN);
+    lv_obj_set_style_height(coin_display, LV_SIZE_CONTENT, LV_PART_MAIN);
+    lv_obj_set_style_pad_hor(coin_display, COINS_LABEL_PX_HORIZONTAL_PADDING, LV_PART_MAIN);
+    lv_obj_set_style_pad_ver(coin_display, COINS_LABEL_PX_VERTICAL_PADDING, LV_PART_MAIN);
+
+    // Use flex layout to position children
+    lv_obj_set_layout(coin_display, LV_LAYOUT_FLEX);
+    lv_obj_set_flex_flow(coin_display, LV_FLEX_FLOW_ROW);
+    lv_obj_set_style_pad_column(coin_display, 0, LV_PART_MAIN);
+    
+
+    // Create the white text displaying the number of coins collected
+    coins_label = lv_label_create(coin_display);
     lv_obj_set_style_text_color(coins_label, lv_color_hex(0xffffff), LV_PART_MAIN);
-    lv_obj_set_style_pad_all(coins_label, 2, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(coins_label, LV_OPA_50, LV_PART_MAIN);
+    update_coin_counter();
+
+    // Create the coin sprite image
+    coin_image = lv_image_create(coin_display);
+    lv_image_set_src(coin_image, &SpriteCoin);
+    lv_obj_set_align(coin_image, LV_ALIGN_RIGHT_MID);
 }
 
 bool coin_callback(game_object_t* context) {
@@ -22,7 +59,7 @@ bool coin_callback(game_object_t* context) {
 
     // Update the coin label
     coin_count += 1;
-    lv_label_set_text_fmt(coins_label, "%02d", coin_count);
+    update_coin_counter();
 
     // Don't produce a physical collision
     return false;   
--- app/src/coins.h
+++ app/src/coins.h
@@ -3,8 +3,16 @@
 #include "game_object.h"
 #include "lvgl.h"
 
-#define COINS_LABEL_PX_PADDING 10
+#define COINS_LABEL_PX_MARGIN 10
+#define COINS_LABEL_PX_HORIZONTAL_PADDING 5
+#define COINS_LABEL_PX_VERTICAL_PADDING 5
 
-void init_coin_display(lv_obj_t* game_screen);
+/*
+ * Create the component displaying the number of coins collected
+ */
+void init_coin_display();
 
+/*
+ * Destroy the coin `context` and increment the coin counter
+ */
 bool coin_callback(game_object_t* context);
